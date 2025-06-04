086bb1f65bb2b5d3b03bb4877483f627b58c2e35
Ra3Valik <Ldidi1@yandex.ru>
2025-06-04 18:13:24 +0300

    Add webpack and configure them

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
 <?php
+
+function r3_enqueue_assets()
+{
+	wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/dist/style.css' );
+	wp_enqueue_script( 'theme-script', get_template_directory_uri() . '/dist/main.js', [], null, true );
+}
+
+add_action( 'wp_enqueue_scripts', 'r3_enqueue_assets' );
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: [
+            './assets/js/main.js',
+            './assets/scss/load.scss',
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
+    devtool: 'source-map',
+};
